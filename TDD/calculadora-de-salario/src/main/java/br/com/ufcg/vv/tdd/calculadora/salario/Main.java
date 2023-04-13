a7473bd52a7d06cb65b14acc40743119200de5aa
Pedro Manoel <pedro.alves@ccc.ufcg.edu.br>
2023-04-13 12:54:05 -0300

    ajusta groupid do projeto

--- TDD/calculadora-de-salario/src/main/java/br/com/ufcg/vv/tdd/calculadora/salario/Main.java
+++ TDD/calculadora-de-salario/src/main/java/br/com/ufcg/vv/tdd/calculadora/salario/Main.java
@@ -1,4 +1,4 @@
-package br.com.ufcg.vv.tdd;
+package br.com.ufcg.vv.tdd.calculadora.salario;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
