679f5715a05ef9a02dc7cce7c105802423bdd070
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2023-04-13 21:25:09 -0300

    Refactor do filtro faturas

--- TDD/filtro-de-faturas/src/main/java/br/com/ufcg/vv/tdd/filtro/fatura/FiltroFaturas.java
+++ TDD/filtro-de-faturas/src/main/java/br/com/ufcg/vv/tdd/filtro/fatura/FiltroFaturas.java
@@ -6,30 +6,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FiltroFaturas {
+    private static final double VALOR_MINIMO = 2000.0;
+    private static final double VALOR_MEDIO = 2500.0;
+    private static final double VALOR_ALTO = 3000.0;
+    private static final double VALOR_RS = 4000.0;
+
+    private static boolean valorMenorQueMinimo(Fatura fatura) {
+        return fatura.getValor() < VALOR_MINIMO;
+    }
+
+    private static boolean valorEntreMinimoEMedioEDataAntiga(Fatura fatura) {
+        LocalDate umMesAtras = LocalDate.now().minus(Period.ofMonths(1));
+        return fatura.getValor() >= VALOR_MINIMO && fatura.getValor() <= VALOR_MEDIO && fatura.getData().isBefore(umMesAtras);
+    }
+
+    private static boolean valorEntreMedioEAltoEDataInclusaoAntiga(Fatura fatura) {
+        LocalDate doisMesesAtras = LocalDate.now().minus(Period.ofMonths(2));
+        return fatura.getValor() > VALOR_MEDIO && fatura.getValor() <= VALOR_ALTO && fatura.getCliente().getDataInclusao().isBefore(doisMesesAtras);
+    }
+
+    private static boolean valorMaiorQueRS(Fatura fatura) {
+        return fatura.getValor() > VALOR_RS && fatura.getCliente().getEstado().equals("RS");
+    }
+
     public List<Fatura> filtraFaturas(List<Fatura> faturas) {
         List<Fatura> faturasFiltradas = new ArrayList<>();
 
-        LocalDate umMesAtras = LocalDate.now().minus(Period.ofMonths(1));
-        LocalDate doisMesesAtras = LocalDate.now().minus(Period.ofMonths(2));
-
-        for (Fatura f : faturas) {
-            if (f.getValor() < 2000.0) {
-                continue;
+        for (Fatura fatura : faturas) {
+            if (!valorMenorQueMinimo(fatura) && !valorEntreMinimoEMedioEDataAntiga(fatura) && !valorEntreMedioEAltoEDataInclusaoAntiga(fatura) && !valorMaiorQueRS(fatura)) {
+                faturasFiltradas.add(fatura);
             }
-
-            if (f.getValor() >= 2000.0 && f.getValor() <= 2500.0 && f.getData().isBefore(umMesAtras)) {
-                continue;
-            }
-
-            if (f.getValor() > 2500.0 && f.getValor() <= 3000.0 && f.getCliente().getDataInclusao().isBefore(doisMesesAtras)) {
-                continue;
-            }
-
-            if (f.getValor() > 4000.0 && f.getCliente().getEstado().equals("RS")) {
-                continue;
-            }
-
-            faturasFiltradas.add(f);
         }
 
         return faturasFiltradas;
